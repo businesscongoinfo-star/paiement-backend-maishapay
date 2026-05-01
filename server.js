d4783f15ef0bff518a8fb4fc11b375dcfddad84f
businesscongoinfo-star <businesscongo.info@gmail.com>
2026-05-01 23:49:25 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// test serveur
+app.get("/", (req, res) => {
+  res.send("Serveur OK ✅");
+});
+
+// route paiement
+app.post("/payer", async (req, res) => {
+  const { phone, amount } = req.body;
+
+  try {
+    const response = await fetch("https://api.maishapay.online/api/payment", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + process.env.API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        amount: amount,
+        phone: phone,
+        currency: "XAF"
+      })
+    });
+
+    const data = await response.json();
+
+    res.json({
+      message: "Paiement envoyé 📱",
+      data: data
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      message: "Erreur serveur ❌"
+    });
+  }
+});
+
+app.listen(3000, () => console.log("Serveur lancé 🚀"));
